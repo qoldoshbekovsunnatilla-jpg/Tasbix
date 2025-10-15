e6f00f6dc79bda7e885be372e348f5420ff914a5
SUNNATILLA <qoldoshbekovsunnat@gmail.com>
2025-10-15 07:46:25 +0500

    yangilandi

--- app.js
+++ app.js
@@ -8,12 +8,13 @@ let inn =document.querySelector(".inn")
 inn.onclick= function() {
 
     num.innerHTML =number++
-    
+    tb.style.backgroundColor = "rgba(0, 128, 0, 0.462)";
 }
 let res =document.querySelector(".res")
 
 res.onclick= function() {
     number=0
     num.innerHTML =number
+    tb.style.backgroundColor= "   rgba(255, 0, 0, 0.315)  " ;
     
 }
